64b47e5be1214c42074e0d1df32ed06067019860
konghao <konghao8013@163.com>
2015-06-07 17:29:10 +0800

    command

--- Command/dal.bat
+++ Command/dal.bat
@@ -1,3 +1,6 @@
-copy ..\Model\bin\debug\model.dll ..\DALSERVER\bin\DALSERVER.dll
+copy ..\Model\bin\debug\model.dll ..\DALSERVER\bin\model.dll
+
+copy ..\Model\bin\debug\model.dll ..\efjs\bin\Debug\model.dll
+
 start ../efjs/bin/Debug/EFJS.exe DAL ../valuating/Resource/Scripts/dbserver ../DALSERVER/bin/Debug/DALSERVER.dll Yes
 pause
